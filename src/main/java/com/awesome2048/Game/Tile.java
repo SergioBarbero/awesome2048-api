b2f04df3892c28de40e68cbff218ec327c21046d
Sergio Barbero <sergio.barbero.bascones95@gmail.com>
2019-05-12 22:33:36 +0200

    Skeleton built

--- src/main/java/com/awesome2048/Game/Tile.java
+++ src/main/java/com/awesome2048/Game/Tile.java
@@ -12,6 +12,6 @@ public class Tile {
     }
 
     public void increase() {
-        this.value = this.value * 2;
+        this.value *= 2;
     }
 }
--- src/main/java/com/awesome2048/Score/ScoreController.java
+++ src/main/java/com/awesome2048/Score/ScoreController.java
@@ -29,11 +29,7 @@ public class ScoreController {
 
     @PostMapping("/move")
     public Score postMove(@RequestBody Score newScore, HttpServletRequest request) throws IOException {
-        String query = "http://api.ipinfodb.com/v3/ip-country/?key=62ee2a10303261af0cf55d6eb2c807c8db5e6fa539fe5ba843c341f4062bfaea&format=json&ip=" + request.getRemoteAddr();
-        String countryCode = ExternalService.readFieldFromGetRequest(query, "countryCode");
-        newScore.setCountryCode(countryCode);
-        repository.save(newScore);
-        return newScore;
+
     }
 
     @PostMapping("/new-game")
